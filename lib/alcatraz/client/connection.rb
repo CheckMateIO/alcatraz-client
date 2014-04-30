48c13f399abd91032fb474508c606401649845c4
Brian McManus <brian@checkmate.io>
2014-04-30 14:45:53 -0700

    Initial commit

---
+++ lib/alcatraz/client/connection.rb
@@ -0,0 +1,96 @@
+require 'faraday_middleware'
+
+module Alcatraz
+  module Client
+    class Connection
+      attr_accessor *Configuration::VALID_OPTIONS
+
+      def initialize(options = {})
+        merged_options = ::Alcatraz::Client.options.merge(options)
+
+        Configuration::VALID_OPTIONS.each do |key|
+          public_send("#{key}=", merged_options[key])
+        end
+      end
+
+      def get_card(id)
+        parse_response_to_secure_object(get("/cards/#{id}"))
+      end
+
+      def store_card!(params)
+        card = parse_response_to_secure_object(post('/cards', params))
+        authorize_data_for_client!(card, public_key)
+        card
+      end
+
+      def get_data(id)
+        parse_response_to_secure_object(get("/secure_data/#{id}"))
+      end
+
+      def store_data!(params)
+        data = parse_response_to_secure_object(post('/secure_data', params))
+        authorize_data_for_client!(data, public_key)
+        data
+      end
+
+      def create_client!(name)
+        parse_response_to_secure_object(post('/api_clients', name: name))
+      end
+
+      def authorize_data_for_client!(data_or_id, client_or_public_key)
+        data_id = unwrap_to_id data_or_id
+        public_key = unwrap_to_id client_or_public_key
+        post("/secure_data/#{data_id}/authorizations", public_key: public_key)
+      end
+
+      def deauthorize_data_for_client!(data_or_id, client_or_public_key)
+        data_id = unwrap_to_id data_or_id
+        public_key = unwrap_to_id client_or_public_key
+        delete("/secure_data/#{data_id}/authorizations/#{public_key}")
+      end
+
+      private
+
+      def unwrap_to_id(object_or_id)
+        object_or_id = object_or_id.id unless object_or_id.kind_of?(String)
+        object_or_id
+      end
+
+      def connection
+        Faraday.new(url: api_url, headers: {'ACCESS_KEY' => public_key}) do |conn|
+          conn.request :json
+          conn.use Faraday::Request::Hmac, secret_key, nonce: (Time.now.to_f * 1e6).to_i.to_s
+          conn.use Faraday::Response::Mashify
+          conn.response :json, content_type: /\bjson$/
+          conn.response :raise_error
+          conn.adapter Faraday.default_adapter
+        end
+      end
+
+      def get(path, options = {})
+        connection.get(path, options)
+      end
+
+      def post(path, options = {})
+        connection.post(path, options)
+      end
+
+      def delete(path)
+        response = connection.delete(path)
+        response.success?
+      end
+
+      def parse_response_to_secure_object(response)
+        if response.success?
+          if response.body.respond_to? :card
+            response.body.card
+          else
+            response.body.secure_datum
+          end
+        else
+          nil
+        end
+      end
+    end
+  end
+end
